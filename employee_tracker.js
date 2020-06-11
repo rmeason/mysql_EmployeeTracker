63f4b0a2c36fa9c7dd32f93ba14d2fe617287436
Richard Michael Eason <rme15a@acu.edu>
2020-06-11 01:41:53 -0500

    removed line

--- employee_tracker.js
+++ employee_tracker.js
@@ -1,6 +1,5 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var consoleTable = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -19,34 +18,34 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
 
-    // start();
+    start();
     console.log("connected");
 });
 
-// function start() {
-//     inquirer
-//       .prompt({
-//         name: "querySelector",
-//         type: "list",
-//         message: "Would you like to search by [ARTIST], those with [MULTIPLE] hits, within a specific [RANGE], or by [SONG]?",
-//         choices: ["ARTIST", "MULTIPLE", "RANGE", "SONG"]
-//       })
-//       .then(function(answer) {
-//         // based on their answer, either call the bid or the post functions
-//         if (answer.querySelector === "ARTIST") {
-//           showArtist();
-//         }
-//         else if(answer.querySelector === "MULTIPLE") {
-//           showMultiple();
-//         } 
-//         else if(answer.querySelector === "RANGE") {
-//             showRange();
-//         }
-//         else if(answer.querySelector === "SONG") {
-//            showSong();
-//         }
-//         else{
-//           connection.end();
-//         }
-//       });
-//     }
+function start() {
+    inquirer
+      .prompt({
+        name: "querySelector",
+        type: "list",
+        message: "Would you like to search by [ARTIST], those with [MULTIPLE] hits, within a specific [RANGE], or by [SONG]?",
+        choices: ["ARTIST", "MULTIPLE", "RANGE", "SONG"]
+      })
+      .then(function(answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.querySelector === "ARTIST") {
+          showArtist();
+        }
+        else if(answer.querySelector === "MULTIPLE") {
+          showMultiple();
+        } 
+        else if(answer.querySelector === "RANGE") {
+            showRange();
+        }
+        else if(answer.querySelector === "SONG") {
+           showSong();
+        }
+        else{
+          connection.end();
+        }
+      });
+    }
