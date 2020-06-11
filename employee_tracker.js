87924a54a093bc0dfa762a71c185de98b0e60a19
Richard Michael Eason <rme15a@acu.edu>
2020-06-11 01:12:25 -0500

    set up port and got it connected

---
+++ employee_tracker.js
@@ -0,0 +1,52 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var consoleTable = require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "12345",
+  database: "employee_tracker_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+
+    // start();
+    console.log("connected");
+});
+
+// function start() {
+//     inquirer
+//       .prompt({
+//         name: "querySelector",
+//         type: "list",
+//         message: "Would you like to search by [ARTIST], those with [MULTIPLE] hits, within a specific [RANGE], or by [SONG]?",
+//         choices: ["ARTIST", "MULTIPLE", "RANGE", "SONG"]
+//       })
+//       .then(function(answer) {
+//         // based on their answer, either call the bid or the post functions
+//         if (answer.querySelector === "ARTIST") {
+//           showArtist();
+//         }
+//         else if(answer.querySelector === "MULTIPLE") {
+//           showMultiple();
+//         } 
+//         else if(answer.querySelector === "RANGE") {
+//             showRange();
+//         }
+//         else if(answer.querySelector === "SONG") {
+//            showSong();
+//         }
+//         else{
+//           connection.end();
+//         }
+//       });
+//     }
